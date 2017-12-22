6ad0a8560225734cc9c291eed1f7682e009fbceb
Jefferson Lara <jefferson.arturo.lara.molina.sa@everis.com>
2017-12-22 17:53:25 -0500

    style(webpack): configuracion de empaquetado

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/main.ts'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bcp-core.bundle.js',
+    libraryTarget: "umd",
+    library:"bcp-core"
+  },
+   resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+     	{ 
+     		test: /\.ts$/, 
+     		loader: 'ts-loader'
+    	}
+    ]
+  }
+};

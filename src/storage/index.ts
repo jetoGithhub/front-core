9c427ff5c89632fce813181304ea57b83dc1f820
Jefferson Lara <jetox21@gmail.com>
2018-01-21 02:13:19 -0500

    feat(storge/index): agregnado el metodo clear a la interfaz de stirages y modificando el metodo remove de la calse abstracta baseBrowserStorage

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -2,6 +2,7 @@ export interface BaseStorage< K, V> {
     get(key: K): V | undefined;
     set(key: K, value: V): void;
     clear(): void;
+    remove(key: string): void;
     /*key(index: number): string | null;
     readonly length: number;
     remove(key: string): void;
@@ -9,18 +10,20 @@ export interface BaseStorage< K, V> {
     [key: K]: any;*/
 }
 
-export abstract class BaseBrowserStorage implements BaseStorage< string, string > {
+export abstract class BaseBrowserStorage implements BaseStorage< string, (string | object) > {
     windowStorage: Storage;
 
     get(key: string) { return this.windowStorage.getItem(key) || undefined; }
     set(key: string, value: string) { this.windowStorage.setItem(key, value); }
-    clear() { this.windowStorage.destroy(); }
+    clear() { this.windowStorage.clear(); }
+    remove(key: string){ this.windowStorage.removeItem(key); }
 }
 
 export abstract class BaseMemoryStorage implements BaseStorage< string, string > {
-    memoryStorage: Object;
+    memoryStorage: Map<string, string>;
 
-    get(key: string) { return this.memoryStorage[key]; }
-    set(key: string, value: string) { return this.memoryStorage[key] = value; }
-    clear() { return this.memoryStorage = {} }
+    get(key: string) { return this.memoryStorage.get(key); }
+    set(key: string, value: string) { return this.memoryStorage.set(key,value); }
+    clear() { return this.memoryStorage.clear(); }
+    remove(key: string){ this.memoryStorage.delete(key); }
 }
